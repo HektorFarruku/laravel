5720a332c8f5e27efaf44102bed506800d60b05d
hektor <hektorfarruku@gmail.com>
2024-05-21 23:54:31 +0200

    Initial commit

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/moduli', function () {
+    return view('moduli');
+});
 Route::resource('users', 'UserController');
 Route::resource('posts', 'PostController');
